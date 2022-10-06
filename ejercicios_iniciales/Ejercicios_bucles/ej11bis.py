56923ae86fb7de50fa2c1a3b0ca0bb036b5831fb
elian <eliandeniz27@gmail.com>
2022-10-06 21:38:28 +0200

    cambios

--- ejercicios_iniciales/Ejercicios_bucles/ej11bis.py
+++ ejercicios_iniciales/Ejercicios_bucles/ej11bis.py
@@ -5,5 +5,5 @@ una las letras de la palabra introducida empezando por la última.
 
 palabra = str(input("Introduzca una palabra para ser leida letra a letra -> "))
 
-for i in range(0, palabra.__len__()):
+for i in range(palabra.__len__()-1, -1, -1):
     print(palabra[i])
