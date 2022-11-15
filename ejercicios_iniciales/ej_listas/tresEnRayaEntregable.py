b590ac09e5b6355360f935116f428287d1505a5c
elian <eliandeniz27@gmail.com>
2022-11-15 17:41:57 +0100

    3 en raya casi terminado

--- ejercicios_iniciales/ej_listas/tresEnRayaEntregable.py
+++ ejercicios_iniciales/ej_listas/tresEnRayaEntregable.py
@@ -5,29 +5,61 @@ detectando cuando se realiza un tres en raya. (utilizar caracteres O para un jug
 otro).
 '''
 
-
-def finPartida():
-    if partida[0:2] == ["O", "0", "0"]:
+# Turnos de cada jugador
+def turnoJ1():
+    if partida[int(input1[0])][int(input1[2])] == "X":
+        partida[int(input1[0])][int(input1[2])] = "0"
         return True
 
 
-partida = [["X", "X", "X"], ["X", "X", "X"], ["X", "X", "X"]]
+def turnoJ2():
+    if partida[int(input2[0])][int(input2[2])] == "X":
+        partida[int(input2[0])][int(input2[2])] = "#"
+        return True
 
+#Formas de ganar
+def ganaFila():
+    if partida[0] == ["0", "0", "0"] or partida[1] == ["0", "0", "0"] or partida[2] == ["0", "0", "0"]:
+        return True
+
+
+def ganaColumna():
+    if partida[0] == ["0", "0", "0"]:
+        return True
+
+
+def ganaDiagonal():
+    if partida[0] == ["0", "0", "0"]:
+        return True
+
+
+# Condicion de finalizar partida
+def finPartida():
+    if ganaFila() or ganaColumna() or ganaDiagonal():
+        return True
+
+
+# Codigo:
+partida = [["X", "X", "X"], ["X", "X", "X"], ["X", "X", "X"]]
 print("\n", partida[0], "\n", partida[1], "\n", partida[2])
 
 # introducir variable guia??? -> 0,0 | 0,1 | 0,2 | 1,0 | 1,1 | 1,2 | 2,0 | 2,1 | 2,2
 # introducir try ex para opciones de bucle fuera de rango
-input1 = input("Introduzca posicion -> ")
-input2 = input("Introduzca posicion -> ")
+
 
 while not finPartida():
-    if partida[int(input1[0])][int(input1[2])] == "0" or partida[int(input1[0])][int(input1[2])] == "#":
-        input1 = input("Posicion ya alterada, introduzca una nueva -> ")
-    else:
-        partida[int(input1[0])][int(input1[2])] = "0"
+    input1 = input("J1: Introduzca posicion -> ")
+    input2 = input("J2: Introduzca posicion -> ")
+    if turnoJ1():
+        turnoJ1()
         print("\n", partida[0], "\n", partida[1], "\n", partida[2])
-        if partida[int(input2[0])][int(input2[2])] == "0" or partida[int(input1[0])][int(input1[2])] == "#":
-            input1 = input("Posicion ya alterada, introduzca una nueva -> ")
-        else:
-            partida[int(input2[0])][int(input2[2])] = "#"
-            print("\n", partida[0], "\n", partida[1], "\n", partida[2])
+    else:
+        input1 = input("Posicion j1 ya alterada, introduzca una nueva -> ")
+
+    if turnoJ2():
+        turnoJ2()
+        print("\n", partida[0], "\n", partida[1], "\n", partida[2])
+    else:
+        input2 = input("Posicion j2 ya alterada, introduzca una nueva -> ")
+
+print("*** FIN PARTIDA ***")
